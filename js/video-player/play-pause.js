50bf072d80ecc1aee2115430ccb380fdb51faf9c
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-08-04 09:35:29 -0300

    🏗 feat: video controls - autoplay

--- js/video-player/play-pause.js
+++ js/video-player/play-pause.js
@@ -1,5 +1,6 @@
 const videoPlayPause = document.querySelector(".video__video");
 const playPause = document.querySelector(".video__controls__play-pause");
+const autoPlay = document.querySelector(".video__controls__autoplay");
 
 setTimeout(() => (videoPlayPause.muted = false), 100);
 
@@ -7,4 +8,8 @@ playPause.addEventListener("click", () => {
   playPause.classList.toggle("video__controls__play-pause--paused");
 
   videoPlayPause.paused ? videoPlayPause.play() : videoPlayPause.pause();
+});
+
+autoPlay.addEventListener("click", () => {
+  autoPlay.classList.toggle("video__controls__autoplay--off");
 });
