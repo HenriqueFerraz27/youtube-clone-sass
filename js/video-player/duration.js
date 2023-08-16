72c86b17f355759cb01a20a342f63adbc9a73fb3
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-08-15 19:40:37 -0700

    🏗 feat: timeline

--- js/video-player/duration.js
+++ js/video-player/duration.js
@@ -1,13 +1,31 @@
 const videoDuration = document.querySelector(".video__video");
 const currentTimeElement = document.querySelector(".video__controls__duration__current-time");
 const totalTimeElement = document.querySelector(".video__controls__duration__total-time");
+const timelineContainer = document.querySelector(".video__timeline-container");
+const timeline = document.querySelector(".video__timeline");
+console.log(timeline);
 
+timelineContainer.addEventListener("mousemove", (e) => {
+  timelineContainer.style.setProperty("--preview-position", `${videoPercent(e)}%`)
+})
+timelineContainer.addEventListener("click", (e) => {
+  videoDuration.currentTime = (videoDuration.duration * videoPercent(e)) / 100
+})
+
+const videoPercent = (e) => {
+  const rect = timelineContainer.getBoundingClientRect()
+  return (Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width) * 100
+}
 
 videoDuration.addEventListener("loadeddata", () => {
   totalTimeElement.textContent = formatDuration(videoDuration.duration)
 })
 
 videoDuration.addEventListener("timeupdate", () => {
+  const videoProgress = (videoDuration.currentTime / videoDuration.duration) * 100;
+
+  timelineContainer.style.setProperty("--progress-position", `${Math.round(videoProgress)}%`)
+
   currentTimeElement.textContent = formatDuration(videoDuration.currentTime)
 })
 
--- js/video-player/play-pause.js
+++ js/video-player/play-pause.js
@@ -3,18 +3,22 @@ const playPause = document.querySelector(".video__controls__play-pause");
 const autoPlay = document.querySelector(".video__controls__autoplay");
 const captionsBtn = document.querySelector(".video__controls__captions");
 
-setTimeout(() => (videoPlayPause.muted = false), 100);
+setTimeout(() => (videoPlayPause.muted = false), 1000);
 
-playPause.addEventListener("click", () => {
+const handlePlayPause = () => {
   playPause.classList.toggle("video__controls__play-pause--paused");
 
   videoPlayPause.paused ? videoPlayPause.play() : videoPlayPause.pause();
-});
+}
+
+videoPlayPause.addEventListener("click", handlePlayPause);
+
+playPause.addEventListener("click", handlePlayPause);
 
 autoPlay.addEventListener("click", () => {
   autoPlay.classList.toggle("video__controls__autoplay--off");
 });
 
 captionsBtn.addEventListener("click", () => {
-  captionsBtn.classList.toggle("video__controls__captions--off");
+  captionsBtn.classList.toggle("video__controls__captions--on");
 });
